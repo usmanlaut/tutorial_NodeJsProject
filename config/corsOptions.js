b0feb7fb4e43abab9d34304341959e5ca23f4f63
usmanlaut <310syrf@gmail.com>
2025-05-12 17:05:01 +0800

    end of tutor 11

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,19 +1,14 @@
-const whitelist = [
-    "https://www.yoursite.com",
-    "http://127.0.0.1:5500",
-    "https://3500-cs-762634953526-default.cs-asia-southeast1-yelo.cloudshell.dev",
-  ];
+const allowedOrigins = require("./allowedOrigins");
 
-  const corsOptions = {
-    origin: (origin, callback) => {
-      if (whitelist.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    optionsSuccessStatus: 200,
-  };
-  
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
 
-module.exports = corsOptions;  
+module.exports = corsOptions;
--- controllers/authController.js
+++ controllers/authController.js
@@ -7,7 +7,7 @@ const usersDB = {
 const bcrypt = require("bcrypt");
 
 const jwt = require("jsonwebtoken");
-require('dotenv').config();
+require("dotenv").config();
 const fsPromises = require("fs").promises;
 const path = require("path");
 
@@ -24,24 +24,31 @@ const handleLogin = async (req, res) => {
   if (match) {
     // create JWTs
     const accessToken = jwt.sign(
-      { "username": foundUser.username },
+      { username: foundUser.username },
       process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: '30s' }
+      { expiresIn: "30s" }
     );
-    const refereshToken = jwt.sign(
-      { "username": foundUser.username },
-      process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: '1d' }
+    const refreshToken = jwt.sign(
+      { username: foundUser.username },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: "1d" }
     );
     // Saving refreshToken with current user
-    const otherUsers = usersDB.users.filter(person => person.username !== foundUser.username);
+    const otherUsers = usersDB.users.filter(
+      (person) => person.username !== foundUser.username
+    );
     const currentUser = { ...foundUser, refreshToken };
     usersDB.setUsers([...otherUsers, currentUser]);
     await fsPromises.writeFile(
       path.join(__dirname, "..", "model", "users.json"),
       JSON.stringify(usersDB.users)
     );
-    res.cookie('jwt', refreshToken, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true,
+      sameSite: "None",
+      secure: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
     res.json({ accessToken });
   } else {
     res.sendStatus(401);
