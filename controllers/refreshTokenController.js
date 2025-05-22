3d4e8bd13695157d247d91c1c841ff9a431aa771
usmanlaut <310syrf@gmail.com>
2025-05-22 23:15:33 +0800

    update roles list, try to revise refresh token but still receive same result

--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -11,6 +11,7 @@ const handleRefreshToken = (req, res) => {
   const cookies = req.cookies;
   if (!cookies?.jwt) return res.sendStatus(401);
   const refreshToken = cookies.jwt;
+  console.log(cookies.jwt);
 
   const foundUser = usersDB.users.find(
     (person) => person.refreshToken === refreshToken
