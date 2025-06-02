45b59c1e11651c248bd77b9ab36d88f9649864ea
usmanlaut <310syrf@gmail.com>
2025-06-02 13:14:54 +0800

    done with tutor 12, just leave console in verifyRoles

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -14,14 +14,18 @@ const handleNewUser = async (req, res) => {
     return res
       .status(400)
       .json({ message: "Username and password are required." });
-  // check for duplicate username in the db
+  // check for duplicate usernames in the db
   const duplicate = usersDB.users.find((person) => person.username === user);
   if (duplicate) return res.sendStatus(409); //Conflict
   try {
     //encrypt the password
     const hashedPwd = await bcrypt.hash(pwd, 10);
     //store the new user
-    const newUser = { username: user, password: hashedPwd };
+    const newUser = {
+      username: user,
+      roles: { User: 2001 },
+      password: hashedPwd,
+    };
     usersDB.setUsers([...usersDB.users, newUser]);
     await fsPromises.writeFile(
       path.join(__dirname, "..", "model", "users.json"),
