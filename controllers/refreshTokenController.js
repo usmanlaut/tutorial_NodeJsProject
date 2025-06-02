ac034c8bebf8342758a5e1c9d0f018a751b08232
usmanlaut <310syrf@gmail.com>
2025-06-02 13:14:54 +0800

    done with tutor 12, just leave console in verifyRoles

--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -11,7 +11,6 @@ const handleRefreshToken = (req, res) => {
   const cookies = req.cookies;
   if (!cookies?.jwt) return res.sendStatus(401);
   const refreshToken = cookies.jwt;
-  console.log(cookies.jwt);
 
   const foundUser = usersDB.users.find(
     (person) => person.refreshToken === refreshToken
@@ -21,8 +20,14 @@ const handleRefreshToken = (req, res) => {
   jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
     if (err || foundUser.username !== decoded.username)
       return res.sendStatus(403);
+    const roles = Object.values(foundUser.roles);
     const accessToken = jwt.sign(
-      { username: decoded.username },
+      {
+        UserInfo: {
+          username: decoded.username,
+          roles: roles,
+        },
+      },
       process.env.ACCESS_TOKEN_SECRET,
       { expiresIn: "30s" }
     );
